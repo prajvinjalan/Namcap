7af86f8cba8cfa03fea10d7a91504748936230cb
Vatsal Shukla <shuklv2@mcmaster.ca>
2016-10-18 20:57:07 -0400

    Added the game border as a barrier. Player only able to turn when within a grid square.

--- src/model/Player.java
+++ src/model/Player.java
@@ -5,7 +5,7 @@ public class Player {
 	char prevDirection, newDirection;
 	int prevX, prevY, currX, currY, sq;
 	boolean stop;
-	int pixelInc = 3;
+	int pixelInc = 2;
 	Board board;
 	
 	public Player(Board board){
@@ -16,46 +16,75 @@ public class Player {
 		this.prevX = 10*this.sq;
 		this.prevY = 15*this.sq;
 		this.currX = 10*this.sq;
-		this.currY = 15*this.sq+4;
+		this.currY = 15*this.sq;
 		this.prevDirection = 'R';
 		this.newDirection = 'R';
+		
+	}
+	
+	public boolean isBarrier(int x, int y){
+		if (x<=380 && y<=380 && x>=20 && y>=20 /*!this.board.getBarrier(x/20-1, y/20-1)*/){
+			return true;
+		}
+		return false;
 		
 	}
 	
 	public void move(){
 		this.prevX = this.currX;
 		this.prevY = this.currY;
-		
-		if (this.currX % 20 == 0 && this.currY % 20 == 0)
-		
-		switch(this.newDirection){
+		if (this.currX % 20 == 0 && this.currY % 20 == 0 || 
+				(this.newDirection == 'L' && this.prevDirection == 'R') ||
+				(this.newDirection == 'U' && this.prevDirection == 'D') ||
+				(this.newDirection == 'R' && this.prevDirection == 'L') ||
+				(this.newDirection == 'D' && this.prevDirection == 'U')){
+			
+			switch(this.newDirection){
 			case 'L':
-				this.currX -= this.pixelInc;
+				if (this.isBarrier(this.currX - pixelInc, this.currY)){
+					this.currX -= this.pixelInc;
+				}
 				break;
 			case 'R':
-				this.currX += this.pixelInc;
+				if (this.isBarrier(this.currX + pixelInc, this.currY)){
+					this.currX += this.pixelInc;
+				}
 				break;
 			case 'U':
-				this.currY -= this.pixelInc;
+				if (this.isBarrier(this.currX, this.currY - this.pixelInc)){
+					this.currY -= this.pixelInc;
+				}
 				break;
 			case 'D':
-				this.currY += this.pixelInc;
+				if (this.isBarrier(this.currX, this.currY + this.pixelInc)){
+					this.currY += this.pixelInc;
+				}
 				break;
+		}
+			
 		}
 		
 		if (this.prevX == this.currX && this.prevY == this.currY){
 			switch(this.prevDirection){
 				case 'L':
-					this.currX -= this.pixelInc;
+					if (this.isBarrier(this.currX - pixelInc, this.currY)){
+						this.currX -= this.pixelInc;
+					}
 					break;
 				case 'R':
-					this.currX += this.pixelInc;
+					if (this.isBarrier(this.currX + pixelInc, this.currY)){
+						this.currX += this.pixelInc;
+					}
 					break;
 				case 'U':
-					this.currY -= this.pixelInc;
+					if (this.isBarrier(this.currX, this.currY - this.pixelInc)){
+						this.currY -= this.pixelInc;
+					}
 					break;
 				case 'D':
-					this.currY += this.pixelInc;
+					if (this.isBarrier(this.currX, this.currY + this.pixelInc)){
+						this.currY += this.pixelInc;
+					}
 					break;
 			}
 		}else{

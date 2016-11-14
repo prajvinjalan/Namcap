9da0206376a15f4c9699cf501721e9b83d896867
Baltej Singh Toor <toorbs@mcmaster.ca>
2016-11-13 23:11:44 -0500

    Documentation of Controller Classes

--- src/controller/ButtonController.java
+++ src/controller/ButtonController.java
@@ -1,3 +1,11 @@
+/**
+* @file ButtonController.java
+* @title ButtonController
+* @data 13/11/2016
+* @brief This class handles any user input to buttons.
+* @details This class handles any user input to the buttons of the application (primarily the main menu). The input pertains only to the implemented 
+* buttons of the program.
+*/
 package controller;
 
 import java.awt.event.*;
@@ -5,16 +13,38 @@ import javax.swing.*;
 
 import model.MainMenu;
 
+/**
+* Button Controller class
+* 
+* This class handles any user input to the buttons of the application. The input pertains only to the implemented
+* butons of the program.
+*/
 public class ButtonController implements ActionListener{
 
+	/**
+	* Corresponding main menu for this instance of the program.
+	*/
 	private MainMenu menu;
+	/**
+	* Corresponding JButton implemented on the main menu.
+	*/
 	private JButton button;
 	
+	/**
+	* @brief Constructor for a Button Controller
+	* @details Constructor accepts two parameters for both the corresponding main menu and Jbutton objects.
+	* @param MainMenu menu corresponding to the implemented button.
+	* @param JButton button corresponding to the implemented menu (frame).
+	*/
 	public ButtonController(MainMenu menu, JButton button){
 		this.menu = menu;
 		this.button = button;
 	}
 	
+	/**
+	* @brief Handles button action occurrence
+	* @param ActionEvent e representing to the button press.
+	*/
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (this.button.getText() == "Start Game"){
--- src/controller/KeyController.java
+++ src/controller/KeyController.java
@@ -1,3 +1,10 @@
+/**
+* @file KeyController.java
+* @title KeyController
+* @data 13/11/2016
+* @brief This class handles any user key inputs.
+* @details This class handles all user key inputs (primarily used for the movement of the player). Maps the corresponding keys to player movement.
+*/
 package controller;
 
 import java.awt.event.*;
@@ -5,14 +12,32 @@ import javax.swing.*;
 
 import model.Board;
 
+/**
+* Key Controller Class
+*
+* This class handles all user key inputs (primarily used for the movement of the player). Maps the corresponding keys to player movement.
+*/
 public class KeyController implements KeyListener{
 
+	/**
+	* Corresponding game board object for the this instance of the program.
+	*/
 	private Board board;
 	
+	/**
+	* @brief Constructor for a Key Controller
+	* @details Constructor accepts one parameter for the corresponding board object for the controller.
+	* @param Board board corresponding to the instance key controller.
+	*/
 	public KeyController(Board board){
 		this.board = board;
 	}
 
+	/**
+	* @brief Handles key press events
+	* @details Handles key press events and maps to the correct player movement direction.
+	* @param KeyEvent e representing the current key press.
+	*/
 	@Override
 	public void keyPressed(KeyEvent e) {
 		switch(e.getKeyCode()){
@@ -32,12 +57,18 @@ public class KeyController implements KeyListener{
 		this.board.updateView();
 	}
 
+	/**
+	* @brief Method required to be included for Key Events
+	*/
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	* @brief Method required to be included for Key Events
+	*/
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub

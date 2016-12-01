5bd3715fd65c1e3a03cf2a96e48b030bcb55250b
prajvinjalan <jalanp@mcmaster.ca>
2016-12-01 17:50:07 -0500

    Slight modification to instructions page.

--- src/model/Namcap.java
+++ src/model/Namcap.java
@@ -24,7 +24,7 @@ public class Namcap {
 		MainMenu menu = new MainMenu();
 		MainMenuView menuGUI = new MainMenuView(menu, frame);
 		frame.setContentPane(menuGUI);
-		frame.setPreferredSize(new Dimension(450,560));
+		frame.setPreferredSize(new Dimension(450,620));
 		frame.pack();
 		frame.setVisible(true);
 	}
